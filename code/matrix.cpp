ffcf00fde9d7240eef4d76706ec0a979af29a0e3
qiubix <qiubix@gmail.com>
2015-04-13 14:37:29 +0200

    Add matrix code with gif

---
+++ code/matrix.cpp
@@ -0,0 +1,47 @@
+for (i = 0; i < 10; i++) {
+  if (object.value == 0) {
+    performFirstAction();
+  }
+  else if (object.value == 1) {
+    performSecondAction();
+  }
+}
+
+for (i = 0; i < 10; i++) {
+  ift
+}
+
+namespace test {
+  class matrix
+  {
+  public:
+    matrix();
+    ~matrix();
+    int matrix::memberFunction() {
+      for (i = 0; i < 10; i++) {
+        if (input.value == 0){
+          field = 0;
+        }
+        else if (input.value == 1){
+         field = 0;
+        }
+        else if (input.value == 2){
+          field = 2;
+        }
+        else if (input.value == 3){
+          field = 3;
+        }
+        else if (input.value == 4){
+          field = 4;
+        }
+        else if (input.value == 5){
+          field = 5;
+        }
+      }
+    }
+
+  private:
+    int field;
+    std::vector<int> numbers;
+  };
+} /* namespace test */
